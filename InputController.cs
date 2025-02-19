aee11eef7f60218405001f18cf5902684930745e
JulesRuby <JulesRuby@users.noreply.github.com>
2025-02-18 23:13:27 -0700

    Okay, added in some handling cases for this method to avoid empty strings. Also a trycatch so the program doesn't close on exception.

--- InputController.cs
+++ InputController.cs
@@ -17,7 +17,17 @@ namespace IinterfacesAndAbstractClasses
             {
                 Console.Write($"{userPromptMessage} : ");
 
-                string? userInput = Console.ReadLine();
+                string userInput = Console.ReadLine();
+
+                // Using this to help me get around empty userInput
+                //https://learn.microsoft.com/en-us/dotnet/api/system.string.isnullorwhitespace?view=net-9.0
+                if (String.IsNullOrWhiteSpace(userInput))
+                {
+                    userPromptMessage = "You may not enter an empty string, try again";
+                    continue;
+                }
+
+                Console.WriteLine($"userInput Empty? ");
 
                 // I could also declare this T parsedInput within the function call itself to make the variable accessible
                 // however this is a little easier to read and follow, I think. The IDE harsses me to inline it, but I still
@@ -49,18 +59,24 @@ namespace IinterfacesAndAbstractClasses
         // Learned about a cool way of running type conversions using Convert.ChangeType(), lets see if I can get this working
         // Should be a cool experiment I can carry foward in a useful way for future input validation methods
         // https://learn.microsoft.com/en-us/dotnet/api/system.convert.changetype?view=net-9.0
-        internal static bool ParseUserInput<T>(string? userInput, out T parsedInput)
+        internal static bool ParseUserInput<T>(string userInput, out T? parsedInput)
         {
-            // Should return the parsed value of type T parsedInput, or if failure to parse, null
-            parsedInput = (T)Convert.ChangeType(userInput, typeof(T));
 
-            if (parsedInput != null)
+
+            // OKay so good to know the compiler will ext the program if I don't leverage a try-catch to 
+            // handle the exception thrown by trying to parse an incorrect data type.
+            try
             {
+                // Should return the parsed value of type T parsedInput, or if failure to parse, null
+                parsedInput = (T)Convert.ChangeType(userInput, typeof(T));
                 return true;
-            } else
+               
+            } catch
             {
+                parsedInput = default!;
                 return false;
             }
+
         }
 
     }
